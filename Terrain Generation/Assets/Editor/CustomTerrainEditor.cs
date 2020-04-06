b78d81dcb76ed2140fe03dfe68deab40bde31c01
Tim <tgaskell23@gmail.com>
2020-04-06 16:52:05 +1000

    Lessons 1- 11 + Comments
    - Introduction in the terrain object in Unity,
    - Created Terrain by Manual.
    - Started to create custom gui for random heights on terrain

---
+++ Terrain Generation/Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EditorGUITable;
+using UnityEditor;
+
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+public class CustomTerrainEditor : Editor
+{
+    //Properties ------
+    SerializedProperty randomHeightRange;
+
+    //Fold Outs ------
+    bool showRandom = false;
+
+    private void OnEnable() {
+
+        randomHeightRange = serializedObject.FindProperty("randomHeightRange"); //Gets random Height range from linked script in CustomEditor
+    }
+
+    /// <summary>
+    /// Method used for creating a modified Unity Editor. Linked to Custom Terrain (Shown up top)
+    /// </summary>
+    public override void OnInspectorGUI() {
+
+        serializedObject.Update();
+
+        CustomTerrain terrain = (CustomTerrain)target;
+
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random");
+       
+        if (showRandom) {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider); // Creates Break between items
+            GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel); // Text label
+            EditorGUILayout.PropertyField(randomHeightRange); //SerializedProperty to be edited
+           
+            if(GUILayout.Button("Random Heights")) { //Generates Button 
+                terrain.RandomTerrain(); //If button pressed will execute code
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Terrain Generation/Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class CustomTerrain : MonoBehaviour
+{ 
+    public Vector2 randomHeightRange = new Vector2(0, 0.1f);
+
+
+    private void Awake() {
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        //Apply tag changes to tag database
+        tagManager.ApplyModifiedProperties();
+
+        //take this object
+        this.gameObject.tag = "Terrain";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Adds a Tag into the Unity Editor.
+    /// </summary>
+    /// <param name="tagsProp"> Tag Manager for unity </param>
+    /// <param name="newTag"> Name of tag being created </param>
+    void AddTag(SerializedProperty tagsProp, string newTag) {
+
+        bool found = false;
+
+        //Ensure the tag doesn't already exist
+        for(int i = 0; i < tagsProp.arraySize; i++) {
+
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            if(t.stringValue.Equals(newTag)) { found = true; break; }
+        }
+
+        //Add new tag
+        if (!found) {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
+        }
+    }
+
+    public void RandomTerrain() {
+
+    }
+}
